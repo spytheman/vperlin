5c2f8cbf6fd5e3d139893453fdebcc82154d6b77
Delyan Angelov <delian66@gmail.com>
2019-09-18 13:11:22 +0300

    Initialization.

---
+++ examples/simple.v
@@ -0,0 +1,9 @@
+import spytheman.perlin
+
+fn main(){
+	//	println( perlin.perm )
+	println( perlin.perm.len )
+
+	println( perlin.noise( 0.0, 0.1, 0.2 ) )
+	println( perlin.noise( 1.2, 0.1, 0.21 ) )
+}
